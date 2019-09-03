c7012f85cffd1691ef4f72b03dabb08421b95c31
Olajide Osho <olajideosho1995@gmail.com>
2019-09-03 10:29:45 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by DigitalLab MacOne on 03/09/2019.
+//  Copyright © 2019 Weilai Jishu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
